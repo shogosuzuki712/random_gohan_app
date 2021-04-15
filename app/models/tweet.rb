8890794a9a68543ed330cc340cdbbc355ceb894a
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-04-15 11:59:42 +0900

    tweetモデルバリデーション

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,11 @@
 class Tweet < ApplicationRecord
+
+  with_options presence: true do
+    validates :title
+    validates :text
+    validates :image
+  end
+  
   belongs_to :user
   has_one_attached :image
 end
