af771965a568bb9bcd57477b4c0b910fe66ac9bf
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-04-15 19:34:18 +0900

    画像投稿機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,11 +5,19 @@ class TweetsController < ApplicationController
   end
 
   def new
-    
+    @tweet = Tweet.new
   end
-
+  
   def create
-  
+    @tweet = Tweet.new(tweet_params)
+    unless @tweet.save
+      render :new
+    end
   end
   
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:title, :text, :image).merge(user_id: current_user.id)
+  end
 end
