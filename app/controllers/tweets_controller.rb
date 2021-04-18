7bd092583674a373e3b461d12f0ad132096a5f67
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-04-18 23:30:18 +0900

    編集削除機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
   def index
   
   end
@@ -14,7 +15,23 @@ class TweetsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
   
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to user_path(current_user.id)
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+  end
+
   private
 
   def tweet_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
-  resources :tweets, only: [:new, :create]
+  resources :tweets, only: [:new, :create, :edit, :update, :destroy]
+  resources :users, only: :show
 end
