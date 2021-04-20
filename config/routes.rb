29db88591baf952f8807e3db3065aeb7ed6f13b6
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-04-20 17:01:09 +0900

    検索機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
-  resources :tweets, only: [:new, :create, :edit, :update, :destroy]
   resources :users, only: :show
+  resources :tweets, only: [:new, :create, :edit, :update, :destroy] do
+    member do
+      get 'search'
+    end
+  end
 end
