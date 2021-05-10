a13e16b818a40ecb4c5dedf0c737e8ca9b69188c
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-05-10 21:27:14 +0900

    インデントの調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tweets = @user.tweets.order("created_at DESC")
+    @tweets = @user.tweets.order('created_at DESC')
   end
 end
