786c68270fcd8f5108a4f929bda76d1882073fc1
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-05-24 17:41:57 +0900

    user新規登録できる時テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -8,12 +8,25 @@ RSpec.describe 'ユーザーが新規登録', type: :system do
   context 'ユーザーが新規登録できるとき' do
     it '正しい情報を入力すればユーザー新規登録ができてトップページに遷移する' do
       # トップページに移動する
+      visit root_path
       # トップページに新規登録ページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
       # 新規登録ページへ遷移する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'user_name', with: @user.name
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
+      fill_in 'user_password_confirmation', with: @user.password_confirmation
       # 新規登録ボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
       # トップページへ遷移したことを確認する
+      expect(current_path).to eq(root_path)
       # 新規登録画面へ遷移するボタンやログインページに遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
     end
   end
   context 'ユーザーが新規登録できないとき' do
