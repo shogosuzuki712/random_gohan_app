447412f10f353cc557260542130747b839123683
shogosuzuki712 <lv_fd2@yahoo.co.jp>
2021-05-25 10:49:24 +0900

    userログインできるときテスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -53,19 +53,28 @@ RSpec.describe 'ユーザーが新規登録', type: :system do
 end
 
 RSpec.describe 'ログイン', type: :system do
-  bifore do
-    @user = FacoryBot.create(:user)
+  before do
+    @user = FactoryBot.create(:user)
   end
 
   context 'ログインできるとき' do
     it '保存されているユーザー情報と合致すればログインができる' do
       # トップページに移動する
+      visit root_path
       # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
       # ログインページへ遷移する
+      visit new_user_session_path
       # 正しいユーザー情報を入力する
+      fill_in 'user_email', with: @user.email
+      fill_in 'user_password', with: @user.password
       # ログインボタンを押す
+      find('input[name="commit"]').click
       # トップページへ遷移することを確認する
+      expect(current_path).to eq(root_path)
       # 新規登録画面へ遷移するボタンやログインページに遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('ログイン')
+      expect(page).to have_no_content('新規登録')
     end
   end
   context 'ログインできないとき' do
